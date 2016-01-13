ed1fbbe3b50b1c420beb4ec79c27fc80fbca21fb
gryvash <grygorii.vashchenko@gmail.com>
2016-01-13 10:48:34 +0100

    Week 3 assignment of R programming at Coursera

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Assignment of week3 from R programming at Coursera
+## For the matrix x and object which contains the matrix
+## itself and methods to get and set its inverse is created
 
-## Write a short comment describing this function
+## This function creates aforementioned object
+## "get" method returns the stored matrix
+## "set" method puts the new matrix to the object and resets
+## inverse matrix
+## "setInverse" method is used by cacheSolve function to set
+## the inverse of stored matrix
+## "getInverse" method returns the cached inverse matrix
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix())
+{
+        cacheInverse <- NULL
+        set <- function(y) {
+                x <<- y
+                cacheInverse <<- NULL
+        }
+        get <- function() x
+        setInverse <- function(InversedMatrix) cacheInverse <<- InversedMatrix
+        getInverse <- function() cacheInverse
+        list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## This function calculates the inverse of the given matrix
+## or takes is from cache if it is already exists.
+## If matrix is not invertable the warning message is thrown
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        cacheInverse <- x$getInverse()
+        if(!is.null(cacheInverse))
+        {
+                message("getting cached data")
+                return(cacheInverse)
+        }
+        mMatrix <- x$get()
+        if(det(mMatrix) == 0)
+        {
+                message("Inverse matrix does not exist")
+        }
+        else
+        {
+                cacheInverse <- solve(mMatrix, ...)
+                x$setInverse(cacheInverse)
+                cacheInverse
+        }
 }
